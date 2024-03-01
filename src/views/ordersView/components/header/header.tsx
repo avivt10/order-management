39bba729ded13881ffa819be2c1a93a9789e4bbc
Aviv Turgeman <avivturgeman10@gmail.com>
2024-03-01 17:30:59 +0200

    task-3 - fixed header

--- src/views/ordersView/components/header/header.tsx
+++ src/views/ordersView/components/header/header.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import HeaderFilters from './headerFilters/headerFilters';
 import { Button } from 'flowbite-react';
 import axios from 'axios';
-
+import "./header.css"
 interface headerProps{
   fetch: () => Promise<void>;
 }
@@ -28,9 +28,6 @@ const Header = ({fetch} : headerProps)  => {
       console.log(err)
     }
   }
-
-
-
 
   return (
     <>
@@ -59,7 +56,6 @@ const Header = ({fetch} : headerProps)  => {
               <div className="hidden md:flex cursor-pointer items-center justify-center text-3xl text-white caret-transparent" onClick={() => setOpenModal(false)}>
                   <svg height="40px" width="40px" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M 19 15 C 17.977 15 16.951875 15.390875 16.171875 16.171875 C 14.609875 17.733875 14.609875 20.266125 16.171875 21.828125 L 30.34375 36 L 16.171875 50.171875 C 14.609875 51.733875 14.609875 54.266125 16.171875 55.828125 C 16.951875 56.608125 17.977 57 19 57 C 20.023 57 21.048125 56.609125 21.828125 55.828125 L 36 41.65625 L 50.171875 55.828125 C 51.731875 57.390125 54.267125 57.390125 55.828125 55.828125 C 57.391125 54.265125 57.391125 51.734875 55.828125 50.171875 L 41.65625 36 L 55.828125 21.828125 C 57.390125 20.266125 57.390125 17.733875 55.828125 16.171875 C 54.268125 14.610875 51.731875 14.609875 50.171875 16.171875 L 36 30.34375 L 21.828125 16.171875 C 21.048125 15.391875 20.023 15 19 15 z"></path></svg>
                 </div>
-
                 <div className="relative z-10 group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900 hidden md:block">
                   <h4 className="title">הוספת הזמנה</h4>
                   <div aria-hidden="true" className="inset-0 absolute aspect-video border rounded-full -translate-y-1 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-green-500 to-white dark:from-white dark:to-white blur-2xl opacity-25 dark:opacity-5 dark:group-hover:opacity-10"></div>
